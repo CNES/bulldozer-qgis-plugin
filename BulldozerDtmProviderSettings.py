a74d92a216ab7ffa29d0a16398c425ecea83abdd
Alexia Mondot <alexia.mondot@thalesgroup.com>
2024-07-17 12:44:10 +0200

    REFAC update headers

--- BulldozerDtmProviderSettings.py
+++ BulldozerDtmProviderSettings.py
@@ -1,15 +1,24 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of Bulldozer
+# (see https://github.com/CNES/bulldozer-qgis-plugin).
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html for
+# more details.
 
-"""
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
-"""
 
 __author__ = 'CNES'
 __date__ = '2023-04-12'
