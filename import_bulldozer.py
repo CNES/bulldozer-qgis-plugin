4ff38bd69582f019a7c551919c0f91914525b467
Alexia Mondot <alexia.mondot@thalesgroup.com>
2025-01-28 10:00:21 +0100

    BUG fix first import bug

--- import_bulldozer.py
+++ import_bulldozer.py
@@ -22,6 +22,7 @@
 import os.path
 import sys
 import pip
+import importlib
 from pathlib import Path
 
 
@@ -31,7 +32,10 @@ except ImportError:
     print("Bulldozer not found in current python path, trying to find it...")
     venv_folder = os.path.join(os.path.dirname(__file__), "bulldozer-dtm_venv")
     os.environ["PATH"] += f":{os.path.join(venv_folder, 'local', 'bin')}"
-    sys.path.append(venv_folder)
+
+    # Ajouter le chemin au sys.path si nécessaire
+    if venv_folder not in sys.path:
+        sys.path.insert(0, venv_folder)
 
     try:
         from bulldozer.pipeline.bulldozer_pipeline import dsm_to_dtm
@@ -40,6 +44,9 @@ except ImportError:
         print("Bulldozer not found in bulldozer-dtm subdir, trying to install it...")
         os.makedirs(venv_folder, exist_ok=True)
         pip.main(["install", "--target", venv_folder, "bulldozer-dtm"])
+
+        # Forcer le rechargement des modules
+        importlib.invalidate_caches()
         sys.path.insert(0, venv_folder)
 
         try:
@@ -55,6 +62,9 @@ except ImportError:
                                                                        "bulldozer/__init__.py"))
             foo = importlib.util.module_from_spec(spec)
             sys.modules["bulldozer"] = foo
+
+            # Forcer le rechargement des modules
+            importlib.invalidate_caches()
             spec.loader.exec_module(foo)
             import bulldozer
             from bulldozer.pipeline.bulldozer_pipeline import dsm_to_dtm
